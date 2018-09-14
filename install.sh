24869289d3ff08f95457c16653ebfc0670f2e9bf
Szymon Krasuski <szymon@szymon-krasuski.intra.codilime.com>
2018-09-14 14:21:48 +0200

    Changed system setup filename

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository ppa:ansible/ansible 
 sudo apt-get update
 sudo apt-get install -y ansible
-wget "https://raw.githubusercontent.com/Dysproz/FlaskTVRemote/master/system_setup.yml"
-sudo ansible-playbook system-setup.yml
+wget "https://raw.githubusercontent.com/Dysproz/FlaskTVRemote/master/systemSetup.yml"
+sudo ansible-playbook systemSetup.yml
 
 
