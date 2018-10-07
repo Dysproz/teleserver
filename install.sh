6e2bb6c8184434716b3e6b246d4a77948807b581
Szymon <krasuski.szymon.piotr@gmail.com>
2018-10-07 09:45:14 +0200

    Auto approve for adiing ansible repository (No more Enter clicking)

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository ppa:ansible/ansible 
+sudo add-apt-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install -y ansible
 wget "https://raw.githubusercontent.com/Dysproz/FlaskTVRemote/master/systemSetup.yml"
