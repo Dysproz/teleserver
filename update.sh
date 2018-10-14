9c59b5c8a3d1ab0d7b5e9293b149ea4f79224d9f
Szymon <krasuski.szymon.piotr@gmail.com>
2018-10-14 18:50:05 +0200

    Reboot instead of playing with running process again

--- update.sh
+++ update.sh
@@ -3,5 +3,4 @@ rm -rf /usr/local/teleserver/*
 rm -rf /usr/local/teleserver/.git
 git clone https://github.com/Dysproz/teleserver.git /usr/local/teleserver/
 chmod 0777 /usr/local/teleserver/*
-pkill -f teleserver
-nohup python /usr/local/teleserver/run.py &
+reboot
