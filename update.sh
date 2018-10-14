70d66f6242073bd2bb63a7ba850aca1274c421b4
Szymon <krasuski.szymon.piotr@gmail.com>
2018-10-14 15:19:04 +0200

    Added destination to git clone

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 rm -rf /usr/local/teleserver/*
 rm -rf /usr/local/teleserver/.git
 cd /usr/local/
-git clone https://github.com/Dysproz/teleserver.git
+git clone https://github.com/Dysproz/teleserver.git /usr/local/teleserver/
 chmod 0777 /usr/local/teleserver/*
 pkill -f teleserver
 nohup python /usr/local/teleserver/run.py &
