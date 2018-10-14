722bde13498608db4f2a1b9d133cd6427cf8cb34
Szymon <krasuski.szymon.piotr@gmail.com>
2018-10-14 15:03:51 +0200

    Minor fix to update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 rm -rf /usr/local/teleserver/*
+rm -rf /usr/local/teleserver/.git
+cd /usr/local/
 git clone https://github.com/Dysproz/teleserver.git
 chmod 0777 /usr/local/teleserver/*
 pkill -f teleserver
