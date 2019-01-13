a5de61602d86247d71b4d8bd4d39fa220cf4a9c4
Szymon Piotr Krasuski <krasuski.szymon.piotr@gmail.com>
2019-01-13 16:56:23 +0100

    Delete update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf /usr/local/teleserver/*
-rm -rf /usr/local/teleserver/.git
-git clone https://github.com/Dysproz/teleserver.git /usr/local/teleserver/
-chmod 0777 /usr/local/teleserver/*
-reboot
