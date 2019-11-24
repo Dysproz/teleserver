547fc93c7a5c3fd3e485a39d98a6118c2fd17bf1
Szymon <krasuski.szymon.piotr@gmail.com>
2019-11-24 14:27:59 +0100

    Fix installation script

--- IoT_client/install_IoT_client.sh
+++ IoT_client/install_IoT_client.sh
@@ -17,7 +17,7 @@ fi
 # Setup of application files with respect to previous versions
 sudo mkdir /var/lib/teleserver_IoT
 sudo rm -rf /var/lib/teleserver_IoT/app > /dev/null
-sudo chmod -R +x /var/lib/teleserver_IoT/
+sudo chmod -R 777 /var/lib/teleserver_IoT/
 sudo cp -rf $PWD /var/lib/teleserver_IoT/app/
 sudo chmod -R +x /var/lib/teleserver_IoT/app
 touch /var/lib/teleserver_IoT/data.yml
@@ -30,7 +30,7 @@ if [ $(grep -o "teleserver_IoT" ~/.profile | wc -l) = 0 ]; then
 	echo "nohup python3 /var/lib/teleserver_IoT/app/IoT_run.py &" >> ~/.profile
 fi
 
-/var/lib/teleserver_IoT/app/tools/secret_managers.py
+/var/lib/teleserver_IoT/app/tools/secret_manager.py
 
 echo "Teleserver IoT client has been installed successfully. Now, restart your session to enjoy IoT client."
 exit 0
